1ffec9d37bf55c1b67bbaa30eece78bfbd000fac
Jeferson Souza <mp_bra_nco@hotmail.com>
2026-02-25 23:48:44 -0300

    Criando a visualização

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from logic.home_logic import obter_mensagem_home
+
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     mensagem = obter_mensagem_home()
-    return render_template("home.html", mensagem=mensagem)
+    return render_template("home.html")
+
+
+@app.route("/plano_corte")
+def plano_corte():
+    return render_template("plano_corte.html")
+
+@app.route("/necessidade_prod")
+def necessidade_prod():
+    return render_template("necessidade_prod.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ logic/home_logic.py
@@ -0,0 +1,2 @@
+def obter_mensagem_home():
+    return "Sistema funcionando corretamente"
